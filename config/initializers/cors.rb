e4150a96ffb74777e6a7c56a576b9b4f311ed290
Ankita <ankitachourasiya11@gmail.com>
2024-02-09 22:05:06 +0530

    Added cross origin configure

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # or specify your frontend domain e.g., 'localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
